556c525087daaa448648d03561ef29d81d10696c
Yash Pandey <mspr1129@orange.com>
2020-08-04 12:16:48 +0530

    Updates

---
+++ precache-manifest.5eceac7bbfd08b54b5a126309bd6e8cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd55918209dc5fc35b2b6313c6b3291",
+    "url": "/yashpandey73/index.html"
+  },
+  {
+    "revision": "7e1ca3ad3f2f5c202da0",
+    "url": "/yashpandey73/static/css/2.861f4f67.chunk.css"
+  },
+  {
+    "revision": "af84cbe801fe61a3b3ce",
+    "url": "/yashpandey73/static/css/main.1ec9115e.chunk.css"
+  },
+  {
+    "revision": "7e1ca3ad3f2f5c202da0",
+    "url": "/yashpandey73/static/js/2.2d643787.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yashpandey73/static/js/2.2d643787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af84cbe801fe61a3b3ce",
+    "url": "/yashpandey73/static/js/main.ea8ed857.chunk.js"
+  },
+  {
+    "revision": "5f46caf961ea8fe673de",
+    "url": "/yashpandey73/static/js/runtime-main.b0b7b4fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yashpandey73/static/media/logo.5d5d9eef.svg"
+  }
+]);
